74597cc3610c9e846af4717b6cb1656e965dee74
Tran Huong Lan <20lan.th@vinuni.edu.vn>
2021-12-21 16:16:59 +0700

    linkedlist and hashtable

--- hashtable.c
+++ hashtable.c
@@ -4,6 +4,9 @@
 #include "hashtable.h"
 
 struct hashtable {
+    int num_buckets;
+    int size;
+    linkedlist_t **array;
     // TODO: define hashtable struct to use linkedlist as buckets
 };
 
@@ -17,19 +20,33 @@ static int hash(int key, int max_range) {
 }
 
 hashtable_t *ht_init(int num_buckets) {
+    hashtable_t *table = malloc(sizeof(hashtable_t));
+    table -> array = malloc(num_buckets * sizeof(linkedlist_t*));
+    for (int i = 0; i < num_buckets; i++)
+        table -> array[i] = ll_init();
+    table -> size = 0;
+    table -> num_buckets = num_buckets;
+    return table;
     // TODO: create a new hashtable
 }
 
 void ht_add(hashtable_t *table, int key, int value) {
+    int bucket = hash(key, table -> num_buckets);
+    int size = ll_size(table -> array[bucket]);
+    ll_add(table -> array[bucket], key, value);
+    if (ll_size(table -> array[bucket]) > size) table -> size++;
     // TODO: create a new mapping from key -> value.
     // If the key already exists, replace the value.
 }
 
 int ht_get(hashtable_t *table, int key) {
+    int bucket = hash(key, table -> num_buckets);
+    return ll_get(table -> array[bucket], key);
     // TODO: retrieve the value mapped to the given key.
     // If it does not exist, return 0
 }
 
 int ht_size(hashtable_t *table) {
+    return table -> size;
     // TODO: return the number of mappings in this hashtable
 }
--- linkedlist.c
+++ linkedlist.c
@@ -6,13 +6,18 @@
 struct linkedlist
 {
     struct linkedlist_node *first;
+    int size;
     // TODO: define linked list metadata
 };
 
 struct linkedlist_node
 {
+    int key;
+    int value;
+    struct linkedlist_node *next;
     // TODO: define the linked list node
 };
+
 typedef struct linkedlist_node linkedlist_node_t;
 
 linkedlist_t *ll_init()
@@ -21,7 +26,7 @@ linkedlist_t *ll_init()
 
     // We have done this TODO for you as an example of how to use malloc().
     // You might want to read more about malloc() from Linux Manual page.
-    // Usually free() should be used together with malloc(). For example, 
+    // Usually free() should be used together with malloc(). For example,
     // the linkedlist you are currently implementing usually have free() in the
     // ll_delete() function. Since we are not asking you to implement
     // the ll_delete() function, you will not be using free() in this case.
@@ -32,12 +37,28 @@ linkedlist_t *ll_init()
     // malloc returns a void pointer to the memory you just allocated, but
     // we can assign this pointer to the specific type we know we created
     linkedlist_t *list = malloc(sizeof(linkedlist_t));
-
+    list->first = NULL;
+    list->size = 0;
+    return list;
     // TODO: set metadata for your new list and return the new list
 }
 
 void ll_add(linkedlist_t *list, int key, int value)
 {
+    linkedlist_node_t *current = list->first;
+    while (current != NULL){
+            if (current->key == key){
+                current->value = value;
+                return;
+            }
+            current = current->next;
+    }
+    linkedlist_node_t *newNode = malloc(sizeof(linkedlist_node_t));
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = list->first;
+    list->first = newNode;
+    list->size++;
     // TODO: create a new node and add to the front of the linked list if a
     // node with the key does not already exist.
     // Otherwise, replace the existing value with the new value.
@@ -45,6 +66,14 @@ void ll_add(linkedlist_t *list, int key, int value)
 
 int ll_get(linkedlist_t *list, int key)
 {
+    linkedlist_node_t *current = list->first;
+    while (current != NULL){
+        if (current->key == key){
+        return current->value;
+        current = current->next;
+        }
+    }
+    return 0;
     // TODO: go through each node in the linked list and return the value of
     // the node with a matching key.
     // If it does not exist, return 0.
@@ -52,5 +81,6 @@ int ll_get(linkedlist_t *list, int key)
 
 int ll_size(linkedlist_t *list)
 {
-    // TODO: return the number of nodes in this linked list
+        // TODO: return the number of nodes in this linked list
+    return list->size;
 }
